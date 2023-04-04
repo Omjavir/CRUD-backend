36c4d1245fbdab63f746f19799d65b0995f52804
Om Suresh Javir <javirom07042003@gmail.com>
2023-04-04 14:59:33 +0530

    updated url

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect("mongodb://localhost:27017/To-do")
+    .connect(process.env.DB_URL)
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
