7a46d8043c6d8755f33e5b4656ed94ae569109c3
Om Suresh Javir <javirom07042003@gmail.com>
2023-04-05 20:46:02 +0530

    okay now lets check

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: '*'
+    origin: ["http://127.0.0.1:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
